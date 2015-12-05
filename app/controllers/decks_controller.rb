48d042dd9decb37328d328e6645bafac44b98cf3
Arcangelo Petretta <arcpetretta@gmail.com>
2015-12-05 13:07:01 -0500

    adds new deck and cards to seed file, creates '/decks' route and view folder.

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,4 +1,5 @@
 get '/decks' do
+  @decks = Deck.all
   erb :'decks/index'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-Deck.create({name: 'Retro Game Trivia', description: "meh"})
+Deck.create([{name: 'Retro Game Trivia', description: "meh"},{name: 'Sports Trivia', description: "stuff"}])
 
 
 #Retro Game Trivia
 Card.create([{deck_id: 1, question:'What highly addictive puzzle game designed by a Russian programmer features Russian buildings, dancers and music?', answer:'Tetris'},
              {deck_id: 1, question:'Before becoming a plumber in the 1983 game Mario Bros., Mario had a different profession. What was it? ', answer:'Pizza Maker'},
-             {deck_id: 1, question:'Who is Bagman? ', answer:'Retired safe cracker'}])
+             {deck_id: 1, question:'Who is Bagman? ', answer:'Retired safe cracker'},
+             {deck_id: 2, question:'How many Championships did Bill Russell win in his career?', answer:'11'},
+             {deck_id: 2, question:'Who won the last world cup?', answer:'Germany'},
+             {deck_id: 2, question:'Who is the NFL all-time leading rusher?', answer:'Emmitt Smith'}])
