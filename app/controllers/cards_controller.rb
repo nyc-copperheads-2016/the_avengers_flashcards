81b64bbd5c7ddcd75904942f69e0fd0e2202ea02
eshou1014 <eshou1014@gmail.com>
2015-12-05 14:49:25 -0500

    Added card form and compared answer to database

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,3 +1,14 @@
-# get '/card/:id'
-#   card =
-#   erb :
+get '/card/:id' do
+  card = Card.find(params[:id])
+  @card = card
+  erb :'cards/question'
+end
+
+put '/cards/:id' do
+  @card = Card.find(params[:id])
+  if params[:answer] == @card.answer
+    redirect '/'
+  else
+    '/?errors=incorrect_username_or_password'
+  end
+end
