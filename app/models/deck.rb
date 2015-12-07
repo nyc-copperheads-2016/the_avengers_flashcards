1d583dcbaf34b4bc0f31fb422cde1c7f41808e80
Chris Savage <chrislsavage@gmail.com>
2015-12-06 22:54:30 -0500

    fix whitespace and bad redirect action

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,26 +1,9 @@
 class Deck < ActiveRecord::Base
   has_many :rounds
   has_many :cards
-  
-  # # initialize
-  #   @cards = []
 
-  # end
   def self.shuffle(deck)
     (1..deck.cards.count).to_a.shuffle
   end
-
-
-  # #shuffle method
-  #    self.cards.shuffle!
-
-  # #Pick a card
-  #   self.cards.sample
-  # #Correct?
-  #   if card is correct, and its the first time we've seen it 
-  #     increment first correct and pop the card
-  #   else
-  #   end
-
 
 end
--- app/models/guess.rb
+++ app/models/guess.rb
@@ -1,3 +1,3 @@
 class Guess < ActiveRecord::Base
-  # Remember to create a migration!
+
 end
